f48a4ef2dce0b69eacebdfe484fdded5d7144184
angellip48 <angellip48@gmail.com>
2023-02-21 14:21:04 +0700

    webpack-shablon

---
+++ src/js/index.js
@@ -0,0 +1,14 @@
+//  import * as $ from 'jquery'
+ import '../styles/main.scss'
+ import '../index.html'
+//  import '../template.pug'
+
+//  import React from 'react'
+// import { render } from 'react-dom'
+//  import json from './assets/data'
+//  import xml from './assets/data.xml'
+//  import csv from './assets/data.csv'
+
+// const App = () => (
+// )
+// render(<App />, document.getElementById('app'))
